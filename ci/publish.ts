c5e4ce416d366dd7cb662ed0a02ea919573cf05a
Michael Masson <10071388+michaelmass@users.noreply.github.com>
2023-11-05 17:23:09 -0500

    chore(ci): publish docker image (#2)

---
+++ ci/publish.ts
@@ -0,0 +1,39 @@
+import { connect } from "https://esm.sh/@dagger.io/dagger@0.8.7";
+import {
+  build,
+  login,
+  logout,
+  deploy,
+} from "https://raw.githubusercontent.com/michaelmass/pipelines/master/dagger/docker.ts";
+import { getInfinsical } from "https://raw.githubusercontent.com/michaelmass/pipelines/master/dagger/infisical.ts";
+
+await connect(async (client) => {
+  const infisical = getInfinsical({ client });
+
+  const dockerTokenSecret = await infisical.get({
+    name: "TOKEN",
+    secretPath: "docker",
+    secretName: "dockerToken",
+  });
+
+  const dockerUsernameSecret = await infisical.get({
+    name: "USERNAME",
+    secretPath: "docker",
+    secretName: "dockerUsername",
+  });
+
+  await login({
+    username: dockerUsernameSecret,
+    password: dockerTokenSecret,
+  })
+
+  const container = await build({ client });
+
+  await deploy({
+    container,
+    repository: "michaelmass/hellomicro",
+    tags: ["latest"],
+  });
+
+  await logout();
+});
